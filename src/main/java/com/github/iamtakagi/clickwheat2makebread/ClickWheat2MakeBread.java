68893d95fdd6fd4932e36cb820354a4e52259120
iamtakagi <github@iamtakagi.net>
2024-02-20 13:05:43 +0900

    feat: toggle command

--- src/main/java/com/github/iamtakagi/clickwheat2makebread/ClickWheat2MakeBread.java
+++ src/main/java/com/github/iamtakagi/clickwheat2makebread/ClickWheat2MakeBread.java
@@ -1,5 +1,9 @@
 package com.github.iamtakagi.clickwheat2makebread;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,11 +15,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClickWheat2MakeBread extends JavaPlugin {
 
+  private static Map<UUID, Boolean> players = new HashMap<>();
+
   @Override
   public void onEnable() {
+    this.saveDefaultConfig();
+    players = (Map<UUID, Boolean>) this.getConfig().get("players");
     this.getServer().getPluginManager().registerEvents(new Listener() {
       @EventHandler
       public void onClick(PlayerInteractEvent event) {
+        if (!players.getOrDefault(event.getPlayer().getUniqueId(), false)) {
+          return;
+        }
         if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR) {
           ItemStack item = event.getPlayer().getItemInHand();
           if (item == null) {
@@ -32,5 +43,26 @@ public class ClickWheat2MakeBread extends JavaPlugin {
         }
       }
     }, this);
+    this.getCommand("bt").setExecutor((sender, command, label, args) -> {
+      if (sender instanceof Player) {
+        Player player = (Player) sender;
+        UUID uuid = player.getUniqueId();
+        if (players.containsKey(uuid)) {
+          players.put(uuid, !players.get(uuid));
+        } else {
+          players.put(uuid, true);
+        }
+        player.sendMessage("ClickWheat2MakeBread: " + (players.get(uuid) ? "ON" : "OFF"));
+        this.getConfig().set("players", players);
+        this.saveConfig();
+      }
+      return true;
+    });
+  }
+
+  @Override
+  public void onDisable() {
+    this.getConfig().set("players", players);
+    this.saveConfig();
   }
 }
